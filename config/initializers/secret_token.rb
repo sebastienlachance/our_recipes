9f85e180f625ae69edfbf0954388e0d2f44fc6c5
Sebastien Lachance <sebastien.lachance@creatiom.com>
2012-09-05 14:53:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OurRecipes::Application.config.secret_token = 'b2d9511a606cd37c6518db21961ecbd5ee0cf04c3c05ffaf76999597aeff93f77754a93e01daf1b4a9dbdb23a8ec765e1aa5a7a1f098507465316d84c76a3214'
